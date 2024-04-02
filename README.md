455376abad904fd4275a0752262178ac0fa7e450
Adrian <adrian.ciolkiewicz@makimo.pl>
2024-04-02 20:53:18 +0200

    feat: Init application

---
+++ README.md
@@ -0,0 +1,15 @@
+# Kudo Cards
+
+Board for kudo cards with possibility to create new kudos.
+
+## Used Stuff
+
+- Next.js
+- React.js
+- TypeScript
+- Redux Toolkit
+- MongoDB
+- Tailwind CSS
+- Atomic Design
+
+## Deployed version
---
+++ prettierrc.cjs
@@ -0,0 +1,15 @@
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  quoteProps: 'as-needed',
+  semi: true,
+  singleAttributePerLine: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  vueIndentScriptAndStyle: false,
+};
