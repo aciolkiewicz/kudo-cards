e5667354ac5f7df13231fef493bfad74a81a0ceb
Adrian <adrian.ciolkiewicz@makimo.pl>
2024-04-19 00:04:28 +0200

    feat: Prepare slide of quots

--- src/components/Organisms/BottomBar/BottomBar.tsx
+++ src/components/Organisms/BottomBar/BottomBar.tsx
@@ -1,13 +1,23 @@
+import StrongText from "@/components/Atoms/StrongText/StrongText";
 import Typography from "@/components/Atoms/Typography/Typography";
+import { quots } from "@/constants/index";
 
 import styles from "./BottomBar.module.css";
 
 const BottomBar = () => {
   return (
     <footer className={styles.bottomBar}>
-      <Typography>
-        <>Here’s to those who inspire you and don’t even know it.</>
-      </Typography>
+      <section className={styles.carouselContainer}>
+        {quots.map((quot) => (
+          <Typography customClass="textCenter" key={quot.sentence}>
+            <>
+              <StrongText>{`"`}</StrongText>
+              {quot.sentence}
+              <StrongText>{`" - ${quot.author}`}</StrongText>
+            </>
+          </Typography>
+        ))}
+      </section>
     </footer>
   );
 };
