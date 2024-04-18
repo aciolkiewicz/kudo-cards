5aa084f5ac95cfe532b74a31f89e016e5549df97
Adrian <adrian.ciolkiewicz@makimo.pl>
2024-04-18 23:48:00 +0200

    feat: Add quots to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -22,3 +22,43 @@ export const cardColors = [
   { name: "moon-stone" },
   { name: "rich-black" },
 ];
+
+export const quots = [
+  { sentence: "Thank you for being the reason I smile.", author: "Anonymous" },
+  { sentence: "Thank you for being you.", author: "Anonymous" },
+  {
+    sentence: "Here’s to those who inspire you and don’t even know it.",
+    author: "Anonymous",
+  },
+  { sentence: "Thank you for brightening my world.", author: "Anonymous" },
+  { sentence: "Let us be kinder to one another.", author: "Aldous Huxley" },
+  { sentence: "You’ve always believed in me. Thank you!", author: "Anonymous" },
+  {
+    sentence: "Thank you for being an important part of my story.",
+    author: "Anonymous",
+  },
+  {
+    sentence:
+      "Saying thank you is more than good manners, it is good spirituality.",
+    author: "Alfred Painter",
+  },
+  {
+    sentence:
+      "Kindness is a language which the deaf can hear and the blind can see.",
+    author: "Mark Twain",
+  },
+  {
+    sentence:
+      "When I started counting my blessings, my whole life turned around.",
+    author: "Willie Nelson",
+  },
+  {
+    sentence: "There is always, always, always something to be thankful for.",
+    author: "—Anonymous",
+  },
+  {
+    sentence:
+      "When we give cheerfully and accept gratefully, everyone is blessed.",
+    author: "Maya Angelou",
+  },
+];
