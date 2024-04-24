3fe500056aeca0c104be165a4ccd6e0338f6da63
Adrian <adrian.ciolkiewicz@makimo.pl>
2024-04-24 10:00:46 +0200

    feat: Add copy icon with functionality to copy

--- src/components/Atoms/CopyToClipboard/CopyToClipboard.tsx
+++ src/components/Atoms/CopyToClipboard/CopyToClipboard.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
 import Typography from "../Typography/Typography";
 import styles from "./CopyToClipboard.module.css";
 
@@ -6,11 +11,26 @@ interface Properties {
 }
 
 const CopyToClipboard = ({ valueToCopy }: Properties) => {
+  const [copied, setCopied] = useState(false);
+  const copyHandler = () => {
+    setCopied(true);
+    navigator.clipboard.writeText(valueToCopy);
+    setTimeout(() => setCopied(false), 2000);
+  };
+
   return (
     <section className={styles.copyToClipboard}>
       <Typography customClass="cornsilkMarginReset">
         <>{valueToCopy}</>
       </Typography>
+      <Image
+        className={`${styles.copyIcon} ${copied && styles.copiedIcon}`}
+        src={copied ? "/icons/done.svg" : "/icons/copy.svg"}
+        alt="Copy to clipboard"
+        width={25}
+        height={25}
+        onClick={copyHandler}
+      />
     </section>
   );
 };
