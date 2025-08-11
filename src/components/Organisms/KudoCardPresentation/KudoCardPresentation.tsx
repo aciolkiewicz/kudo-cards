e0fac383a9cc726f6f843d5734c6222be49462df
Adrian <adrian.ciolkiewicz@makimo.pl>
2025-08-11 14:24:35 +0200

    feat: add confetti to card preview

--- src/components/Organisms/KudoCardPresentation/KudoCardPresentation.tsx
+++ src/components/Organisms/KudoCardPresentation/KudoCardPresentation.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import confetti from "canvas-confetti";
 import { usePathname } from "next/navigation";
 import { enqueueSnackbar, SnackbarProvider } from "notistack";
 import { useEffect, useState } from "react";
@@ -50,6 +51,34 @@ const KudoCardPresentation = ({ cardId }: Parameters) => {
   useEffect(() => {
     getKudoCard();
   }, [cardId]);
+
+  useEffect(() => {
+    if (kudoCardSaved && "cardTitle" in kudoCardSaved) {
+      const end = Date.now() + 10 * 1000;
+      const colors = ["#bb0000", "#ffffff"];
+
+      (function frame() {
+        confetti({
+          particleCount: 2,
+          angle: 60,
+          spread: 55,
+          origin: { x: 0 },
+          colors: colors,
+        });
+        confetti({
+          particleCount: 2,
+          angle: 120,
+          spread: 55,
+          origin: { x: 1 },
+          colors: colors,
+        });
+
+        if (Date.now() < end) {
+          requestAnimationFrame(frame);
+        }
+      })();
+    }
+  }, [kudoCardSaved]);
 
   if (kudoCardSaved && "cardTitle" in kudoCardSaved) {
     return (
