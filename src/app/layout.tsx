91b9146b7f6df35f17a2b556fdc1662c42f64689
Adrian <adrian.ciolkiewicz@makimo.pl>
2025-11-03 13:43:31 +0100

    feat: change import for atuh0 lib

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,13 @@ import type { Metadata } from "next";
 import { Roboto_Slab } from "next/font/google";
 import { Suspense } from "react";
 
-import { auth0 } from "@/app/lib/auth0";
 import Loading from "@/components/Atoms/Loading/Loading";
 import LoginPopup from "@/components/Molecules/LoginPopup/LoginPopup";
 import BottomBar from "@/components/Organisms/BottomBar/BottomBar";
 import TopBar from "@/components/Organisms/TopBar/TopBar";
 
 import styles from "./layout.module.css";
+import { auth0 } from "./lib/auth0";
 
 const robotoSlab = Roboto_Slab({ subsets: ["latin"] });
 
